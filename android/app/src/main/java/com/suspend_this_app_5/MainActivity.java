a572e2d3dcb048633951fc07ddc981c75faf2c3b
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2022-07-01 21:22:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/suspend_this_app_5/MainActivity.java
@@ -0,0 +1,15 @@
+package com.suspend_this_app_5;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "suspend_this_app_5";
+  }
+}
